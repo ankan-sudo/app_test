0d6f3945d8147330fc17e16679a706adfa516bbe
ankan-sudo <badboys19ster@gmail.com>
2020-10-31 11:29:04 +0530

    Add() written with syso

--- App_test/src/com/csi/git/Addition.java
+++ App_test/src/com/csi/git/Addition.java
@@ -2,7 +2,15 @@ package com.csi.git;
 
 public class Addition {
 	public static void main(String[] args) {
-		System.out.println();
+		Addition a = new Addition();
+		int n=a.add(2, 3);
+		System.out.println(n);
+		System.out.println("Welcome to CSI");
+		
+	}
+	int add(int n1, int n2){
+		int sum = n1+n2;
+		return sum;
 	}
 
 }
